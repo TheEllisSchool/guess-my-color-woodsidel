c41f45d7afb1123e2b8f8cb274a2996f348334cb
woodsidel <woodsidel@LR064WA7-1718.ellis.int>
2019-01-12 17:19:29 -0500

    Finished Layout

--- src/GuessMyColor.java
+++ src/GuessMyColor.java
@@ -1,7 +1,13 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 public class GuessMyColor extends JFrame{
@@ -12,7 +18,7 @@ public class GuessMyColor extends JFrame{
 		initGUI();
 		
 		setTitle("Game Window");
-		setSize(200, 100); //pixels
+		//setSize(200, 100); //pixels
 		setResizable(true);
 		pack();
 		setLocationRelativeTo(null); //centers on screen, do this after packing but before visible
@@ -24,9 +30,63 @@ public class GuessMyColor extends JFrame{
 	}
 	
 	public void initGUI(){
+		//create the label
 		JLabel titleLabel = new JLabel("Guess My Color");
-		add(titleLabel);
+		//customize the label
+		Font titleFont = new  Font ("DubielPlain", Font.BOLD + Font.ITALIC, 32);
+		titleLabel.setFont(titleFont);
+		titleLabel.setForeground(Color.MAGENTA);
+		titleLabel.setBackground(Color.BLACK);
+		titleLabel.setOpaque(true);
+		//add the label to your window
+		add(titleLabel, BorderLayout.PAGE_START);
 		titleLabel.setHorizontalAlignment(JLabel.CENTER); //left or right
+		
+		
+		//CENTER PANEL
+		JPanel centerPanel = new JPanel();
+		centerPanel.setBackground(Color.CYAN);
+		add(centerPanel, BorderLayout.CENTER);
+		
+		Dimension size = new Dimension(50, 50);
+		
+		JPanel myPanel = new JPanel();
+		myPanel.setBackground(Color.BLACK); //this will change with buttons
+		myPanel.setPreferredSize(size);
+		centerPanel.add(myPanel, BorderLayout.PAGE_START);
+		
+		
+		JPanel computerPanel = new JPanel();
+		computerPanel.setBackground(Color.YELLOW); //will have to be randomized color
+		computerPanel.setPreferredSize(size);
+		centerPanel.add(computerPanel, BorderLayout.PAGE_END);
+		
+		//BUTTON PANEL
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBackground(Color.BLACK);
+		add(buttonPanel, BorderLayout.PAGE_END);
+		
+		//buttons
+		JButton moreRedButton = new JButton("+");
+		moreRedButton.setBackground(Color.RED);
+		buttonPanel.add(moreRedButton);
+		JButton lessRedButton = new JButton("-");
+		lessRedButton.setBackground(Color.RED);
+		buttonPanel.add(lessRedButton);
+		
+		JButton moreGreenButton = new JButton("+");
+		moreGreenButton.setBackground(Color.GREEN);
+		buttonPanel.add(moreGreenButton);
+		JButton lessGreenButton = new JButton("-");
+		lessGreenButton.setBackground(Color.GREEN);
+		buttonPanel.add(lessGreenButton);
+		
+		JButton moreBlueButton = new JButton("+");
+		moreBlueButton.setBackground(Color.BLUE);
+		buttonPanel.add(moreBlueButton);
+		JButton lessBlueButton = new JButton("-");
+		lessBlueButton.setBackground(Color.BLUE);
+		buttonPanel.add(lessBlueButton);
 	}
 
 	public static void main(String[] args) {
